21317630b45cb7e8a70b0953b5bea8efd3f12886
kmfrick <kmfrick98@gmail.com>
2020-12-27 15:00:46 +0100

    Add exercise 7

---
+++ 6/MatrixProductMPI.c
@@ -0,0 +1,133 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <mpi.h>
+#include <time.h>
+
+const int MAX = 10;
+
+int main(int argc, char* argv[])
+{
+	MPI_Init(&argc, &argv);
+	if (argc < 2) {
+		printf("Usage: %s MATRIX_SIZE\n", argv[0]);
+		MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
+	}
+	int m = atoi(argv[1]);
+
+	// Get number of processes and check that 4 processes are used
+	int size;
+	int rank;
+	MPI_Comm_size(MPI_COMM_WORLD, &size); // size = num of components of MPI_COMM_WORLD
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank); // rank = process id inside MPI_COMM_WORLD
+	if(size > m)
+	{
+		printf("No. of processes %d is greater than matrix size %d.\n",size, m);
+		MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
+	}
+	if (m % size) {
+		printf("Matrix size %d is not a multiple of process count %d.\n", m, size);
+		MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
+	}
+
+	int rows = m / size;
+	MPI_Bcast(&rows, 1, MPI_INT, 0, MPI_COMM_WORLD);
+
+	// Define my value
+	int *my_A = NULL, *my_C = NULL;
+	int *B = (int*) malloc(m * m * sizeof(int));
+
+	if (rank == 0)
+	{
+		int *A = (int*) malloc(sizeof(int) * m * m); // Ensure matrix is contiguous
+		srand((unsigned int)time(NULL)); 
+		for(int i = 0; i < m; i++) {
+			for (int j = 0; j < m; j++) {
+				A[i * m + j]=rand() % MAX + 1;
+				B[i * m + j]=rand() % MAX + 1;
+			}
+		}
+		/*
+			 printf("A = \n");
+			 for(int i=0; i<m; i++) {
+			 for (int j = 0; j < m; j++) {
+			 printf("\t%d", B[i][j]);
+			 }
+			 printf("\n");
+			 }
+
+			 printf("B = \n");
+			 for(int i=0; i<m; i++) {
+			 for (int j = 0; j < m; j++) {
+			 printf("\t%d", B[i][j]);
+			 }
+			 printf("\n");
+			 }
+			 */
+		// Scatter matrix to processes
+		my_A = (int*)malloc(m * rows * sizeof(int));
+		if (my_A == NULL) {
+			printf("my_A is NULL n process %d\n", rank);
+			MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
+		}
+		MPI_Scatter(A, m * rows, MPI_INT, my_A, m * rows, MPI_INT, 0, MPI_COMM_WORLD);
+	} else  {
+		// Scatter
+		my_A = (int*)malloc(m * rows * sizeof(int));
+		if (my_A == NULL) {
+			printf("my_A is NULL n process %d\n", rank);
+			MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
+		}
+		MPI_Scatter(NULL, m * rows, MPI_INT, my_A, m * rows, MPI_INT, 0, MPI_COMM_WORLD);
+	}
+	MPI_Bcast(B, m*m, MPI_INT, 0, MPI_COMM_WORLD);
+	MPI_Barrier(MPI_COMM_WORLD);
+	double begin = MPI_Wtime();
+
+	my_C = (int*)malloc(m * rows * sizeof(int));
+	if (my_C == NULL) {
+		printf("my_C is NULL n process %d\n", rank);
+	}
+	for(int i = 0; i < rows; i++) {
+		for (int j = 0; j < m; j++) { 
+			my_C[i * m + j] = 0;
+		}
+	}
+
+	for(int i = 0; i < rows; i++) {
+		for (int j = 0; j < m; j++) { 
+			for (int k = 0; k < m; k++) {
+				my_C[i * m + j] += my_A[i * m + k] * B[k * m + j]; 
+			}
+		}
+	}
+
+	if (rank==0) //collettore
+	{   
+		int  C[m][m];
+		MPI_Gather(my_C, m * rows, MPI_INT, C, m * rows, MPI_INT, 0, MPI_COMM_WORLD);
+		/*
+			 printf("Risultato C = AB:\n");
+			 for(int i=0; i<m; i++) {
+			 for (int j = 0; j < m; j++) {
+			 printf("\t%d", C[i][j]);
+			 }
+			 printf("\n");
+			 }
+			 */
+	} else  {
+		MPI_Gather(my_C, m * rows, MPI_INT, NULL, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	}
+	MPI_Barrier(MPI_COMM_WORLD);
+	double end=MPI_Wtime();
+	double local_elaps= end-begin;
+	double global_elaps;
+	MPI_Reduce(&local_elaps, &global_elaps,1,MPI_DOUBLE,MPI_MAX,0,MPI_COMM_WORLD);
+	if (rank == 0) {			
+		printf("n = %d, m = %d, t = %lf\n", size, m, global_elaps);
+	}
+	MPI_Finalize();
+
+	return EXIT_SUCCESS;
+}
+
---
+++ 7/MatrixProductOpenMP.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <omp.h>
+#include <time.h>
+
+const size_t m = 10;
+const int MAX = 10;
+
+int main(int argc, char* argv[])
+{
+
+	if (argc < 2) {
+		printf("Usage: %s MATRIX_SIZE NUM_PROCESSES\n", argv[0]);
+	}
+	int m = atoi(argv[1]);
+	int size = atoi(argv[2]);
+	int rank;
+	if(size > m)
+	{
+		printf("No. of processes %d is greater than matrix size %d.\n",size, m);
+		abort();
+	}
+	if (m % size) {
+		printf("Matrix size %d is not a multiple of process count %d.\n", m, size);
+		abort();
+	}
+
+
+	// Define my value
+	int B[m][m], C[m * m];
+
+	int **A = (int**) malloc(sizeof(int*) * m);
+	int *A_data = (int*) malloc(sizeof(int) * m * m); // Ensure matrix is contiguous
+	memset(A_data, 0, m * m * sizeof(*A_data));
+	for (int i = 0; i < m; i++) {
+		A[i] = &(A_data[m * i]);
+	}
+	srand((unsigned int)time(NULL)); 
+	// Generate A and B
+	for(int i = 0; i < m; i++) {
+		for (int j = 0; j < m; j++) {
+			A[i][j] = rand() % MAX + 1;
+			B[i][j] = rand() % MAX + 1;
+		}
+	}
+	/*
+	printf("A = \n");
+	for(int i = 0; i<m; i++) {
+		for (int j = 0; j < m; j++) {
+			printf("\t%d", B[i][j]);
+		}
+		printf("\n");
+	}
+
+	printf("B = \n");
+	for(int i = 0; i<m; i++) {
+		for (int j = 0; j < m; j++) {
+			printf("\t%d", B[i][j]);
+		}
+		printf("\n");
+	}
+	*/
+
+	// Zero out C
+	for(int i = 0; i < m; i++) {
+		for (int j = 0; j < m; j++) { 
+			C[i * m + j] = 0;
+		}
+	}
+#pragma omp barrier
+	double start = omp_get_wtime(); 
+#pragma omp parallel num_threads(size) shared(A, B, C, m)
+#pragma omp for schedule(static)
+	for(int i = 0; i < m; i++) {
+		for (int j = 0; j < m; j++) { 
+			for (int k = 0; k < m; k++) {
+				C[i * m + j] = C[i * m + j] + (A[i][k] * B[k][j]); 
+			}
+		}
+	}
+#pragma omp barrier
+	double end = omp_get_wtime();
+	printf("n = %d, m = %d, t = %lf\n", size, m, end - start);
+	/*
+	printf("C = AB\n");
+	for(int i = 0; i< m; i++) {
+		for (int j = 0; j < m; j++) {
+			printf("\t%d", C[i * m + j]);
+		}
+		printf("\n");
+	}
+	*/
+	return 0;
+}
+
