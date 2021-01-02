b124e01814651c3b3b91e17f70643356919d49f1
kmfrick <kmfrick98@gmail.com>
2021-01-02 14:52:42 +0100

    Add ex 4.2

---
+++ 4/2020-01-08.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+const numWheels = 4
+const maxSleepTime = 5
+
+func Produce(name string, out chan bool) {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for {
+		time.Sleep(time.Duration(r.Int()%maxSleepTime) * time.Second)
+		fmt.Printf("%s producing.\n", name)
+		out <- true
+	}
+}
+
+func Assemble(name string, pullC chan bool, pullP chan bool, done chan bool) {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for {
+		time.Sleep(time.Duration(r.Int()%maxSleepTime) * time.Second)
+		for i := 0; i < numWheels; i++ {
+			pullC <- true
+			pullP <- true
+			fmt.Printf("%s consuming.\n", name)
+		}
+		done <- true
+	}
+}
+
+func When(cond bool, ch chan bool) chan bool {
+	if cond {
+		return ch
+	}
+	return nil
+}
+
+func Serve(pushCA, pushCB, pushPA, pushPB, pullCA, pullCB, pullPA, pullPB, doneA, doneB, doneSrv chan bool) {
+}
+
+func main() {
+	pushCA := make(chan bool)
+	pushCB := make(chan bool)
+	pushPA := make(chan bool)
+	pushPB := make(chan bool)
+	pullCA := make(chan bool)
+	pullCB := make(chan bool)
+	pullPA := make(chan bool)
+	pullPB := make(chan bool)
+
+	doneA := make(chan bool)
+	doneB := make(chan bool)
+
+	go Produce("PA", pushPA)
+	go Produce("CA", pushCA)
+	go Produce("PB", pushPB)
+	go Produce("CB", pushCB)
+
+	go Assemble("A", pullCA, pullPA, doneA)
+	go Assemble("B", pullCB, pullPB, doneB)
+	
+	const target = 10
+	const maxP = 5
+	const maxC = 6
+	cntCA := 0
+	cntCB := 0
+	cntPA := 0
+	cntPB := 0
+	cntDoneA := 0
+	cntDoneB := 0
+	for ; cntDoneA + cntDoneB < target ; {
+		canPushCA := (cntCA+cntCB < maxC) && cntDoneA <= cntDoneB
+		canPushCB := (cntCA+cntCB < maxC) && cntDoneB < cntDoneA
+		canPushPA := (cntPA+cntPB < maxP) && cntDoneA <= cntDoneB
+		canPushPB := (cntPA+cntPB < maxP) && cntDoneB < cntDoneA
+
+		canPullCA := cntCA > 0 && (cntDoneA <= cntDoneB || cntCB == 0)
+		canPullCB := cntCB > 0 && (cntDoneB < cntDoneA || cntCA == 0)
+		canPullPA := cntPA > 0 && (cntDoneA <= cntDoneB || cntPB == 0)
+		canPullPB := cntPB > 0 && (cntDoneB < cntDoneA || cntPA == 0)
+
+		select {
+		case <-When(canPushCA, pushCA):
+			cntCA++
+		case <-When(canPushPA, pushPA):
+			cntPA++
+
+		case <-When(canPullCA, pullCA):
+			cntCA--
+		case <-When(canPullPA, pullPA):
+			cntPA--
+
+		case <-When(canPullCB, pullCB):
+			cntCB--
+		case <-When(canPullPB, pullPB):
+			cntPB--
+
+		case <-When(canPushCB, pushCB):
+			cntCB++
+		case <-When(canPushPB, pushPB):
+			cntPB++
+
+		case <-doneA:
+			cntDoneA++
+		case <-doneB:
+			cntDoneB++
+		}
+		fmt.Printf("CA = %d; CB = %d; PA = %d; PB = %d\n", cntCA, cntCB, cntPA, cntPB)
+		fmt.Printf("Produced %d A cars and %d B cars\n", cntDoneA, cntDoneB)
+	}
+
+}
