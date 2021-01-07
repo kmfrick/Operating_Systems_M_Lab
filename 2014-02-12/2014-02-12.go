620b2cff97fdf7a05e6113a967a677919fceafe7
kmfrick <kmfrick98@gmail.com>
2021-01-07 18:34:20 +0100

    Fix 2014-02-12.go and allow for natural deadlocks to happen

--- 2014-02-12/2014-02-12.go
+++ 2014-02-12/2014-02-12.go
@@ -6,15 +6,15 @@ import (
 	"time"
 )
 
-func produce(id int, exchangeType int, ch chan int, ack chan int, chPrio chan int, ackPrio chan int) {
+func produce(id int, exchangeType int, ch []chan int, ack chan int, chPrio []chan int, ackPrio chan int) {
 	const maxSleepTime = 8
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 	for {
-		ch <- id
+		ch[exchangeType] <- id
 		fmt.Printf("Producer %v produced request for exchange type %v.\n", id, exchangeType)
 		idCons := <-ack
 		fmt.Printf("Request %v for exchange type %v was received by consumer %v.\n", id, exchangeType, idCons)
-		if ch != chPrio {
+		if &ch != &chPrio {
 			fmt.Printf("Producer %v becoming prioritary\n", id)
 			ch = chPrio
 			ack = ackPrio
@@ -24,20 +24,20 @@ func produce(id int, exchangeType int, ch chan int, ack chan int, chPrio chan in
 	}
 }
 
-func consume(id int, exchangeType int, ch chan int, ack chan int, chPrio chan int, ackPrio chan int) {
+func consume(id int, exchangeType int, ch []chan int, ack chan int, chPrio []chan int, ackPrio chan int) {
 	const maxSleepTime = 8
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 	for {
-		ch <- id
+		ch[exchangeType] <- id
 		fmt.Printf("Consumer %v waiting to consume exchange type %v.\n", id, exchangeType)
 		idProd := <-ack
 		fmt.Printf("Consumer %v consumed production %v for exchange type %v.\n", id, idProd, exchangeType)
-		if ch != chPrio {
+		if &ch != &chPrio {
 			fmt.Printf("Consumer %v becoming prioritary\n", id)
 			ch = chPrio
 			ack = ackPrio
 		}
-		exchangeType = (exchangeType+3 + r.Int()%2) % 6
+		exchangeType = (exchangeType + 3 + r.Int()%2) % 6
 		time.Sleep(time.Second * time.Duration(r.Int()%maxSleepTime))
 	}
 }
@@ -65,7 +65,7 @@ func serve(prodCh chan int, prodAck []chan int, prodChPrio chan int, prodAckPrio
 			prodAck[idProd] <- idCons
 			i++
 			fmt.Printf("Consumed %v requests for exchange type %v\n", i, exchangeType)
-		default:
+			// default:
 			// noop
 		}
 	}
@@ -89,30 +89,41 @@ func main() {
 
 	done := make([]chan int, exchangeTypes)
 
+	consCh := make([]chan int, exchangeTypes)
+	consChPrio := make([]chan int, exchangeTypes)
+	prodCh := make([]chan int, exchangeTypes)
+	prodChPrio := make([]chan int, exchangeTypes)
+
+	consAck := make([]chan int, exchangeTypes*numConsumers)
+	consAckPrio := make([]chan int, exchangeTypes*numConsumers)
+	prodAck := make([]chan int, exchangeTypes*numProducers)
+	prodAckPrio := make([]chan int, exchangeTypes*numProducers)
+
 	for k := 0; k < exchangeTypes; k++ {
-		consCh := make(chan int, bufSz)
-		consChPrio := make(chan int, bufSz)
-		consAck := make([]chan int, numConsumers)
-		consAckPrio := make([]chan int, numConsumers)
+		consCh[k] = make(chan int, bufSz)
+		consChPrio[k] = make(chan int, bufSz)
+		prodCh[k] = make(chan int, bufSz)
+		prodChPrio[k] = make(chan int, bufSz)
+	}
+
+	for k := 0; k < exchangeTypes; k++ {
 
 		for i := 0; i < numConsumers; i++ {
-			consAck[i] = make(chan int)
-			go consume(i, k, consCh, consAck[i], consChPrio, consAckPrio[i])
+			idx := i + k * exchangeTypes
+			consAck[idx] = make(chan int)
+			consAckPrio[idx] = make(chan int)
+			go consume(idx, k, consCh, consAck[idx], consChPrio, consAckPrio[idx])
 		}
 
-		prodCh := make(chan int, bufSz)
-		prodChPrio := make(chan int, bufSz)
-		prodAck := make([]chan int, numProducers)
-		prodAckPrio := make([]chan int, numProducers)
-
 		for i := 0; i < numProducers; i++ {
-			prodAck[i] = make(chan int)
-			prodAckPrio[i] = make(chan int)
-			go produce(i, k, prodCh, prodAck[i], prodChPrio, prodAckPrio[i])
+			idx := i + k * exchangeTypes
+			prodAck[idx] = make(chan int)
+			prodAckPrio[idx] = make(chan int)
+			go produce(idx, k, prodCh, prodAck[idx], prodChPrio, prodAckPrio[idx])
 		}
 
 		done[k] = make(chan int)
-		go serve(prodCh, prodAck, prodChPrio, prodAckPrio, consCh, consAck, k, done[k])
+		go serve(prodCh[k], prodAck, prodChPrio[k], prodAckPrio, consCh[k], consAck, k, done[k])
 	}
 	for k := 0; k < exchangeTypes; k++ {
 		<-done[k]
